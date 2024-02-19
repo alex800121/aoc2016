20fda825d848b1d29131a30f1cd30570ff83c48e
alex800121 <alex800121@hotmail.com>
2024-02-19 08:36:21 +0800

    day11

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,6 +1,50 @@
 module Day1 where
 
+import Data.Bifunctor (Bifunctor (..))
+import Data.Foldable (Foldable (..))
+import Data.Function (on)
+import Data.Ix (Ix (range))
+import Data.List (scanl')
+import Data.List.Split (splitOn)
+import Data.Maybe (fromJust)
+import MyLib (Direction (..), firstRepeat', toIndex, (+&))
+
+type Index = (Int, Int)
+
+type Ins = (Char, Int)
+
+type Pos = (Direction, Index)
+
+toTurn :: Char -> Direction -> Direction
+toTurn 'R' = succ
+toTurn 'L' = pred
+
+turn :: Char -> Pos -> Pos
+turn = first . toTurn
+
+forward :: Int -> Pos -> Pos
+forward n p = second (+& bimap (* n) (* n) (toIndex (fst p))) p
+
+readIns :: Ins -> Pos -> Pos
+readIns = (.) <$> forward . snd <*> turn . fst
+
+allLoc :: [Index] -> [Index]
+allLoc (x : y : xs) = tail (f $ range (m, n)) <> allLoc (y : xs)
+  where
+    m = min x y
+    n = max x y
+    f = if x > y then reverse else id
+
 day1 :: IO ()
 day1 = do
-  -- input <- readFile "input/input1.txt"
-  return ()
+  input <- map ((,) <$> head <*> read @Int . tail) . splitOn ", " <$> readFile "input/input1.txt"
+  print
+    . uncurry ((+) `on` abs)
+    . snd
+    $ foldl' (flip readIns) (North, (0, 0)) input
+  print
+    . fmap (uncurry ((+) `on` abs) . snd)
+    . firstRepeat'
+    . ((:) <$> head <*> allLoc)
+    . map snd
+    $ scanl' (flip readIns) (North, (0, 0)) input
--- src/Day7.hs
+++ src/Day7.hs
@@ -1,6 +1,41 @@
 module Day7 where
 
+import Data.Either
+import Data.List.Split (divvy)
+import Data.Maybe (mapMaybe)
+import MyLib
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+type IP = ([String], [String])
+
+ipParser :: Parser IP
+ipParser =
+  partitionEithers
+    <$> many
+      ( (Left <$> between (char '[') (char ']') (many (satisfy (`notElem` "[]"))))
+          <|> (Right <$> some (satisfy (`notElem` "[]")))
+      )
+
+hasABBA :: String -> Bool
+hasABBA = any f . divvy 4 1
+  where
+    f [a, b, c, d] = a == d && b == c && a /= b
+
+hasABA :: IP -> Bool
+hasABA (x, y) = any (`elem` as) bs
+  where
+    as = map g $ concatMap (filter f . divvy 3 1) y
+    f [a, b, c] = a == c && a /= b
+    g [a, b, _] = [b, a, b]
+    bs = concatMap (divvy 3 1) x
+
 day7 :: IO ()
 day7 = do
-  -- input <- readFile "input/input7.txt"
-  return ()
+  input <- mapMaybe (parseMaybe ipParser) . lines <$> readFile "input/input7.txt"
+  print
+    . length
+    $ filter ((&&) <$> (not . any hasABBA . fst) <*> (any hasABBA . snd)) input
+  print
+    . length
+    $ filter hasABA input
