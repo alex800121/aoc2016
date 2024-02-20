f124a82aae74b558730eccf321e3e0ccf43f72a0
alex800121 <alex800121@hotmail.com>
2024-02-21 05:56:47 +0800

    day14

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,6 +1,38 @@
 module Day13 where
 
+import Data.Bits (popCount)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import MyLib ((+&))
+
+input = 1358
+
+type Index = (Int, Int)
+
+calcOpen :: Int -> Index -> Bool
+calcOpen n (x, y) = x >= 0 && y >= 0 && even (popCount a)
+  where
+    a = x * x + 3 * x + 2 * x * y + y + y * y + n
+
+bfs :: Int -> (Int -> Index -> Bool) -> (Index -> Set Index) -> Set Index -> Set Index -> (Int, Set Index)
+bfs n ended nextF visited next
+  | any (ended n) next = (n, visited')
+  | otherwise = bfs (n + 1) ended nextF visited' next'
+  where
+    visited' = visited `Set.union` next
+    next' = Set.unions (Set.map nextF next) Set.\\ visited'
+
+nextF :: Int -> Index -> Set Index
+nextF n i = Set.fromList $ filter (calcOpen n) $ map (+& i) adjacent
+
+adjacent = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+start = (1, 1)
+
+end = (31, 39)
+
 day13 :: IO ()
 day13 = do
   -- input <- readFile "input/input13.txt"
-  return ()
+  print $ fst $ bfs 0 (const (== end)) (nextF input) Set.empty (Set.singleton start)
+  print $ length $ snd $ bfs 0 (\x _ -> x == 50) (nextF input) Set.empty (Set.singleton start)
--- src/Day14.hs
+++ src/Day14.hs
@@ -1,6 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PackageImports #-}
+
 module Day14 where
+
+import Data.Bifunctor (Bifunctor (..))
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import Data.ByteString.Base16
+import Data.ByteString.Internal (w2c)
+import Data.Char (digitToInt, intToDigit)
+import Data.List (find, findIndices, unfoldr)
+import Data.Maybe (fromJust, isJust)
+import Data.String (IsString (..))
+import qualified "cryptohash-md5" Crypto.Hash.MD5 as MD5
+
+-- input = "abc"
+input = "qzyelonm"
+
+repeatedChar n = map (fromString @ByteString . replicate n . intToDigit) [0 .. 15]
+
+fives = repeatedChar 5
+
+encodeByteString :: ByteString -> (Maybe Int, [Int])
+encodeByteString b = (digitToInt . w2c . B.head <$> find ((>= 3) . B.length) (B.group b), findIndices (`B.isInfixOf` b) fives)
+
+withPostfix :: Int -> Int -> ByteString -> ByteString
+withPostfix i n input = (!! i) $ iterate (encode . MD5.hash) $ input <> fromString (show n)
+
+makeIndices :: [[Int]] -> [[Int]]
+makeIndices xs = map (\x -> findIndices (elem x) xs) l
+  where
+    l = [0 .. 15]
+
+-- calcInput :: (Int -> ByteString -> ByteString) -> ByteString -> Int
+calcInput fn input = filter (`g` i5) i3 
+  where
+    (r3, r5) =
+      unzip $
+        map (encodeByteString . (`fn` input)) [0 ..]
+    f = zip <$> findIndices isJust <*> filter isJust
+    h = zip <$> findIndices (not . null) <*> filter (not . null)
+    i3 = f r3
+    i5 = h r5
+    g (x, y) xs = any (any (== fromJust y) . snd) $ takeWhile ((<= x + 1000) . fst) $ dropWhile ((<= x) . fst) xs
 
 day14 :: IO ()
 day14 = do
-  -- input <- readFile "input/input14.txt"
-  return ()
+  print $ fst $ (!! 63) $ calcInput (withPostfix 1) input
+  -- print $ take 64 $ calcInput (withPostfix 2017) input
+  print $ fst $ (!! 63) $ calcInput (withPostfix 2017) input
