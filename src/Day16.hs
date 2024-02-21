e443e5b62e501c760f751b9beebf4b82ec21b847
alex800121 <alex800121@hotmail.com>
2024-02-21 19:50:59 +0800

    day16

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,6 +1,38 @@
 module Day16 where
+import Data.List (find)
+import Debug.Trace
+
+initInput = "10001001100000001"
+
+l = 272
+l' = 35651584
+
+convertInput = map (== '1')
+step :: [Bool] -> [Bool]
+step = (<>) <$> id <*> (False :) . reverse . map not
+
+checkSum :: [Bool] -> [Bool]
+checkSum xs = f id xs
+  where
+    f acc [] = checkSum (acc [])
+    f acc [x] = xs
+    f acc (x : y : ys) = f (acc . ((x == y) :)) ys
 
 day16 :: IO ()
 day16 = do
   -- input <- readFile "input/input16.txt"
-  return ()
+  -- print initInput
+  print
+    . fmap (map (\x -> if x then '1' else '0') . checkSum . take l)
+    . find ((>= l) . length)
+    . iterate step 
+    $ convertInput initInput
+    -- . fmap (map (\x -> if x then '1' else '0') . checkSum . take 20)
+    -- . find ((>= 20) . length)
+    -- $ iterate step $ convertInput "10000"
+  print
+    . fmap (map (\x -> if x then '1' else '0') . checkSum . take l')
+    . find ((>= l') . length)
+    . iterate step 
+    $ convertInput initInput
+    
