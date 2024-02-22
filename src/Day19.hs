fd67350de9bda949dbe83e965db4781094264502
alex800121 <alex800121@hotmail.com>
2024-02-22 19:54:52 +0800

    day20

--- src/Day19.hs
+++ src/Day19.hs
@@ -1,6 +1,28 @@
 module Day19 where
 
+import Data.Sequence (Seq (..), (|>))
+import qualified Data.Sequence as Seq
+
+input = 3004953 :: Int
+
+initSeq = Seq.fromList [1 .. input]
+
+type Circle = (Int, Seq Int)
+
+day19a :: Seq Int -> Int
+day19a (x :<| y :<| xs) = day19a (xs |> x)
+day19a (x :<| Empty) = x
+
+day19b :: Seq Int -> Int
+day19b (x :<| Empty) = x
+day19b (x :<| xs) = day19b (xs' |> x)
+  where
+    l = Seq.length xs
+    xs' = Seq.deleteAt (((l + 1) `div` 2) - 1) xs
+
 day19 :: IO ()
 day19 = do
   -- input <- readFile "input/input19.txt"
-  return ()
+  -- print $ day19b (Seq.fromList [1 .. 5])
+  print $ day19a initSeq
+  print $ day19b initSeq
--- src/Day20.hs
+++ src/Day20.hs
@@ -1,6 +1,19 @@
 module Day20 where
 
+import Data.List (sort)
+import Data.Word (Word32)
+
+type Range a = (a, a)
+
+findLowest :: (Bounded a, Enum a, Ord a) => a -> [Range a] -> a
+findLowest start = go start . sort
+  where
+    go a [] = a
+    go a (x@(m, n) : xs)
+      | a < m = a
+      | a >= m && a <= n = go (succ (snd x)) xs
+
 day20 :: IO ()
 day20 = do
   -- input <- readFile "input/input20.txt"
-  return ()
+  print (maxBound @Word32)
