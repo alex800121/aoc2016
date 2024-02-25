50904f05c14bf60f5ba32bc186ab53475d7e0253
alex800121 <alex800121@hotmail.com>
2024-02-25 14:11:49 +0800

    done

--- src/Day25.hs
+++ src/Day25.hs
@@ -1,6 +1,14 @@
 module Day25 where
 
+import Numeric (showBin)
+import Data.List (find)
+
+check :: String -> Bool
+check ('1' : '0' : xs) = check xs
+check [] = True
+check _ = False
+
 day25 :: IO ()
 day25 = do
   -- input <- readFile "input/input25.txt"
-  return ()
+  print $ find (check . (`showBin` "") . (+ (182 * 14))) [0..]
