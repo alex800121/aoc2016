ed67c458ea611b1924d64c5fe07a5db415e8c69b
alex800121 <alex800121@hotmail.com>
2024-02-25 13:16:11 +0800

    day24

--- src/Day21.hs
+++ src/Day21.hs
@@ -1,9 +1,38 @@
 module Day21 where
 
+import Data.Foldable (Foldable (..))
+import Data.List (scanl')
 import Data.Sequence (Seq (..))
 import qualified Data.Sequence as Seq
-import Data.Foldable (Foldable(..))
-import Data.List (scanl')
+
+revIns :: String -> Seq Char -> Seq Char
+revIns ins s = case words ins of
+  ["swap", "position", x, _, _, y] ->
+    let a = read x
+        b = read y
+        ca = Seq.index s a
+        cb = Seq.index s b
+     in Seq.update a cb $ Seq.update b ca s
+  ["swap", "letter", x, _, _, y] ->
+    let Just a = Seq.elemIndexL ca s
+        Just b = Seq.elemIndexL cb s
+        ca = head x
+        cb = head y
+     in Seq.update a cb $ Seq.update b ca s
+  ["rotate", "right", x, _] -> uncurry (flip (Seq.><)) $ Seq.splitAt (read x `mod` l) s
+  ["rotate", "left", x, _] -> uncurry (flip (Seq.><)) $ Seq.splitAt (negate (read x) `mod` l) s
+  ["reverse", _, x, _, y] ->
+    let (a', c) = Seq.splitAt (read y + 1) s
+        (a, b) = Seq.splitAt (read x) a'
+     in a Seq.>< Seq.reverse b Seq.>< c
+  ["move", _, y, _, _, x] -> Seq.insertAt (read y) (Seq.index s (read x)) $ Seq.deleteAt (read x) s
+  ["rotate", "based", _, _, _, _, x] ->
+    let Just a = Seq.elemIndexL ca s
+        ca = head x
+        n = [7, 0, 4, 1, 5, 2, 6, 3] !! a
+     in uncurry (flip (Seq.><)) $ Seq.splitAt (negate (n - a) `mod` l) s
+  where
+    l = Seq.length s
 
 readIns :: String -> Seq Char -> Seq Char
 readIns ins s = case words ins of
@@ -37,4 +66,6 @@ readIns ins s = case words ins of
 day21 :: IO ()
 day21 = do
   input <- lines <$> readFile "input/input21.txt"
-  print $ foldl' (flip readIns) (Seq.fromList "abcdefgh") input
+  let a =  foldl' (flip readIns) (Seq.fromList "abcdefgh") input
+  print a
+  print $ foldr revIns (Seq.fromList "fbgdceah") input
--- src/Day24.hs
+++ src/Day24.hs
@@ -1,6 +1,55 @@
 module Day24 where
 
+import Data.Char (digitToInt, isDigit)
+import Data.List (partition, tails, uncons, permutations)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Tuple (swap)
+import MyLib
+
+data S
+  = Space
+  | Point Int
+  deriving (Show, Eq, Ord)
+
+type Index = (Int, Int)
+
+type Edge = (P, P)
+
+type P = (Index, Int)
+
+buildMap :: Map Index S -> Map Edge Int
+buildMap m = Map.unions $ map (uncurry (bfs m)) ps
+  where
+    p = mapMaybe (\(x, y) -> case y of Space -> Nothing; Point i -> Just (x, i)) $ Map.toList m
+    ps = mapMaybe uncons $ tails p
+
+bfs :: Map Index S -> P -> [P] -> Map Edge Int
+bfs m p end = go (Set.singleton (fst p)) end Set.empty 0 Map.empty
+  where
+    go _ [] _ _ acc = acc
+    go start ends visited n acc = go start' ends' visited' (n + 1) acc'
+      where
+        (finished, ends') = partition ((`Set.member` start) . fst) ends
+        start' =
+          Set.filter ((&&) <$> (`Set.notMember` visited') <*> (`Map.member` m))
+            . Set.unions
+            $ map (\x -> Set.map (x +&) start) adjacent
+        visited' = Set.union visited start
+        acc' = Map.union acc $ Map.fromList [((p, b), n) | b <- finished]
+
+adjacent = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+getDistance :: Map Edge Int -> Edge -> Int
+getDistance m e = fromMaybe (m Map.! swap e) (m Map.!? e)
+
 day24 :: IO ()
 day24 = do
-  -- input <- readFile "input/input24.txt"
-  return ()
+  input <- drawMap (\case '.' -> Just Space; x | isDigit x -> Just $ Point (digitToInt x); _ -> Nothing) . lines <$> readFile "input/input24.txt"
+  let p = mapMaybe (\(x, y) -> case y of Space -> Nothing; Point i -> Just (x, i)) $ Map.toList input
+      starts = mapMaybe uncons $ filter ((== 0) . snd . head) $ permutations p
+      m = buildMap input
+  print m
